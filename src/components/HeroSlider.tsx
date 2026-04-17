ce4d421ec2eaea09dfa19747cc0d6c81a58ab0a7
csosamorales <camilasosamorales@gmail.com>
2026-04-17 20:15:24 -0300

    fix: fix build

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 const images = [
@@ -10,12 +9,12 @@ const images = [
 	'/hero-background/background-5.webp',
 ];
 
-const INTERVAL_MS = 5000;
-const TRANSITION_MS = 500;
+// const INTERVAL_MS = 5000;
+// const TRANSITION_MS = 500;
 
 export default function HeroSlider() {
-	const [current, setCurrent] = useState(0);
-	const [fading, setFading] = useState(false);
+	// const [current, setCurrent] = useState(0);
+	// const [fading, setFading] = useState(false);
 
 	// useEffect(() => {
 	// 	const timer = setInterval(() => {
@@ -39,9 +38,9 @@ export default function HeroSlider() {
 					fill
 					priority={index === 0}
 					className="object-cover transition-opacity duration-1000"
-					style={{
-						opacity: index === current ? (fading ? 0 : 1) : 0,
-					}}
+					// style={{
+					// 	opacity: index === current ? (fading ? 0 : 1) : 0,
+					// }}
 				/>
 			))}
 			<div className="absolute inset-0 backdrop-blur-sm" />
